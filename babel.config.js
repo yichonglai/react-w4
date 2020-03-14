6c37810a024db45f21dfc6448716271e2ef9c36d
chonglaiyi <yichonglaicc@gmail.com>
2020-03-14 14:40:20 +0800

    fix:bug

--- babel.config.js
+++ babel.config.js
@@ -18,12 +18,6 @@ const plugins = [
   // 'transform-runtime' 插件告诉 Babel要引用 runtime 来代替注入(辅助代码)。
   // Babel 对一些公共方法使用了非常小的辅助代码，比如 _extend。默认情况下会被添加到每一个需要它的文件中
   "@babel/plugin-transform-runtime",
-  // 按需加载
-  ["import", {
-    "libraryName": "antd",
-    "libraryDirectory": "es",
-    "style": "css" // `style: true` 会加载 less 文件
-  }]
 ];
 
 module.exports = { presets, plugins };
--- src/page/Dashboard/index.tsx
+++ src/page/Dashboard/index.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import style from './index.less';
-import { Button } from 'antd';
 
 interface DashboardProps { };
 interface DashboardState { };
 class Dashboard extends React.PureComponent<DashboardProps, DashboardState> {
   render() {
-    return <div className={style.root}>
-      Dashboard
-      <Button type='primary'>预览按钮</Button>
-    </div>;
+    return <div className={style.root}>Dashboard</div>;
   }
 }
 
