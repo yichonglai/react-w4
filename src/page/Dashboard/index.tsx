247b179db1cbe07b1192471bba62920f3e95929c
chonglaiyi <yichonglaicc@gmail.com>
2020-03-21 18:34:03 +0800

    set home

--- src/page/Dashboard/index.tsx
+++ src/page/Dashboard/index.tsx
@@ -3,10 +3,17 @@ import style from './index.less';
 
 interface IProps { };
 interface IState { };
-class Dashboard extends React.PureComponent<IProps, IState> {
-  render() {
-    return <div className={style.root}>Dashboard</div>;
-  }
+function Dashboard() {
+  return (
+    <div className={style.normal}>
+      <h1 className={style.title}>Hi！Welcome to Yep!</h1>
+      <div className={style.welcome} />
+      <ul className={style.list}>
+        <li>To get started, edit <code>src/index.tsx</code> and save to reload.</li>
+        <li><a href="https://github.com/yichonglai/react-w4/tree/master">Getting Started</a></li>
+      </ul>
+    </div>
+  );
 }
 
 export default Dashboard;
